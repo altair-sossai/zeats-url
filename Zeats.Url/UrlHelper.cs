c149e04c0f6366de224838a5c282aef694c3428d
Altair Sossai <altairsossai@gmail.com>
2021-02-05 15:42:19 -0300

    Inicio do projeto

---
+++ Zeats.Url/UrlHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Zeats.Url
+{
+    public static class UrlHelper
+    {
+        public static bool IsValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+
+        public static bool IsValidHttpUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                   && uriResult.Scheme == Uri.UriSchemeHttp;
+        }
+
+        public static bool IsValidHttpsUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                   && uriResult.Scheme == Uri.UriSchemeHttps;
+        }
+    }
+}
