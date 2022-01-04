1df05877ad88f181545a6998c403ddaa5f10ed01
Altair Sossai <altair_sossai@outlook.com>
2022-01-04 09:49:00 -0800

    upgrade to net6.0

--- Zeats.Url/UrlHelper.cs
+++ Zeats.Url/UrlHelper.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 
-namespace Zeats.Url
+namespace Zeats.Url;
+
+public static class UrlHelper
 {
-    public static class UrlHelper
+    public static bool IsValidUrl(string url)
     {
-        public static bool IsValidUrl(string url)
-        {
-            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
-                   && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-        }
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+               && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+    }
 
-        public static bool IsValidHttpUrl(string url)
-        {
-            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
-                   && uriResult.Scheme == Uri.UriSchemeHttp;
-        }
+    public static bool IsValidHttpUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+               && uriResult.Scheme == Uri.UriSchemeHttp;
+    }
 
-        public static bool IsValidHttpsUrl(string url)
-        {
-            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
-                   && uriResult.Scheme == Uri.UriSchemeHttps;
-        }
+    public static bool IsValidHttpsUrl(string url)
+    {
+        return Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+               && uriResult.Scheme == Uri.UriSchemeHttps;
     }
 }
